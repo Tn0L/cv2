7e9a92b64e9cd33adf50162474ab1203595f3377
Tn Luong <tn0luong@gmail.com>
2021-06-16 15:11:45 +0700

    dit me git

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvComponent } from './pages/cv/cv.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+import { CvListComponent } from './components/cv-list/cv-list.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    HomeComponent,
+    ListComponent,
+    CvListComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
