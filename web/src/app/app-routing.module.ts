87c9a9643c691280a08c1f41dbc7116c1a76b274
Tn Luong <tn0luong@gmail.com>
2021-06-17 13:50:27 +0700

    route

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,23 +1,47 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { BusinessComponent } from './pages/business/business.component';
+import { CvComponent } from './pages/cv/cv.component';
+import { MainComponent } from './components/main/main.component';
+import { FormCvComponent } from './pages/form-cv/form-cv.component';
 
 const routes: Routes = 
-  [{
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'business',
-    component: AdminComponent,
-  },
-  {
-    path: 'admin',
-    component: BusinessComponent,
-  },
-];
+  [
+    {
+      path: '',
+      component: HomeComponent,
+      children: [
+        {
+          path: 'cv',
+          component: MainComponent,
+        },
+      ]
+    },
+    {
+      path: 'client',
+      component: HomeComponent,
+ 
+      children: [
+        {
+          path: 'cv',
+          component: FormCvComponent,
+          outlet: 'client',
+        },
+      ]
+    },
+    {
+      path: '**',
+      component: HomeComponent,
+      children: [
+        {
+          path: '',
+          component: MainComponent,
+        },
+      ]
+    },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { BusinessComponent } from './pages/business/business.component';
 import { AdMenuComponent } from './components/ad-menu/ad-menu.component';
 import { BsmenuComponent } from './components/bsmenu/bsmenu.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { BsmenuComponent } from './components/bsmenu/bsmenu.component';
     FooterComponent,
     BusinessComponent,
     AdMenuComponent,
-    BsmenuComponent
+    BsmenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
